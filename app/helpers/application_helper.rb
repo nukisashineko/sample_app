6f99db53076ef273bbc92c5c8555fb252318321d
nukisashineko <nukisashineko@gmail.com>
2015-11-30 07:52:06 +0900

    refactring rspec

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # get pages full title
   def full_title(page_title)
-    base_title = "Ruby on RAils Tutorial Sample App"
+    base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
       base_title
     else
