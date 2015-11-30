c8c984b46a043b7b8826ab49b1826e8566a2899f
nukisashineko <nukisashineko@gmail.com>
2015-11-30 20:10:37 +0900

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGIX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGIX }, uniqueness: { case_sensitive: false }
-
-
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
