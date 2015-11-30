e373c82ae5a4ada6d221759017d4bb19525e68d2
nukisashineko <nukisashineko@gmail.com>
2015-11-30 09:58:48 +0900

    add user's model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGIX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGIX }, uniqueness: { case_sensitive: false }
+
+
+end
