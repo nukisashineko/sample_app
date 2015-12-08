86fe4a9b161af7188471c37786776b23cdddbd03
nukisashineko <nukisashineko@gmail.com>
2015-12-08 22:09:19 +0900

    add spec update errror

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 include ApplicationHelper
 
 def valid_signin(user)
@@ -22,5 +24,18 @@ end
 RSpec::Matchers.define :have_success_message do |message|
   match do |page|
     expect(page).to have_selector('div.alert.alert-success', text: message )
+  end
+end
+
+def sign_in(user, options={})
+  if options[:no_capybara]
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+  else
+    visit signin_path
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
   end
 end
