4185aba9566e53ffa3e1e98601791906da88baaf
nukisashineko <nukisashineko@gmail.com>
2015-12-28 04:47:12 +0900

    add redirect other user visit update or edit pages

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -30,7 +30,7 @@ end
 def sign_in(user, options={})
   if options[:no_capybara]
     remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
+    cookies[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
   else
     visit signin_path
