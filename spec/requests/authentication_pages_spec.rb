5dc74bf1c92c41126458e48cde1ab9d5b21c4a74
nukisashineko <nukisashineko@gmail.com>
2015-12-28 01:20:58 +0900

    add redirect non-logined user when edit and update

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe "Authentication" do
-  describe "signin page" do
+  subject { page }
 
-    subject { page }
+  describe "signin page" do
 
     describe "signin page" do
       before { visit signin_path }
@@ -36,6 +36,25 @@ describe "Authentication" do
         it { should have_link('Settings', href: edit_user_path(user)) }
         it { should have_link('Sign out', href: signout_path) }
         it { should_not have_link('Sign in', href: signin_path) }
+      end
+    end
+  end
+
+  describe "authorization" do
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+          describe "visiting the edit page" do
+            before { visit edit_user_path user }
+            it { should have_title('Sign in') }
+          end
+
+          describe "submitting to the update action" do
+            before { patch user_path(user) }
+            specify { expect(response).to redirect_to(signin_path)}
+          end
       end
     end
   end
