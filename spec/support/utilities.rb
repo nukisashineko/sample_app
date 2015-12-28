fda39e087d7dea230f2da0dc517a19a522b195a6
nukisashineko <nukisashineko@gmail.com>
2015-12-28 09:01:23 +0900

    exercise 9-4 refactoring rspec use sign_in methods

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -2,12 +2,6 @@ require 'spec_helper'
 
 include ApplicationHelper
 
-def valid_signin(user)
-  fill_in "Email", with: user.email
-  fill_in "Password", with: user.password
-  click_button "Sign in"
-end
-
 def valid_fill_in_signup()
   fill_in "Name", with: "Example User"
   fill_in "Email", with: "user@example.com"
