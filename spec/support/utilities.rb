196c23daae02c4d95ad835ef3a071fe9baea2819
nukisashineko <nukisashineko@gmail.com>
2015-12-28 09:11:55 +0900

    exercise 9-5 refactoring standardize new and edit form

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,7 +6,7 @@ def valid_fill_in_signup()
   fill_in "Name", with: "Example User"
   fill_in "Email", with: "user@example.com"
   fill_in "Password", with: "foobar"
-  fill_in "Confirmation", with: "foobar"
+  fill_in "Confirm Password", with: "foobar"
 end
 
 RSpec::Matchers.define :have_error_message do |message|
