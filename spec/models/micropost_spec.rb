254a0f2b7ea982d8bd963205d216f3e60e7bb6c1
nukisashineko <nukisashineko@gmail.com>
2016-01-05 10:30:10 +0900

    add model microposts presence user_id

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -9,4 +9,10 @@ describe Micropost do
   subject { @microposts }
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
+  it { should be_valid }
+
+  describe "when user id is not present" do
+    before { @microposts.user_id = nil }
+    it { should_not be_valid }
+  end
 end
