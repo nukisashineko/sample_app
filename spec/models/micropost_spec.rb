fe4a16151dd58a9cfdaca405ba0c404d9aa28bf0
nukisashineko <nukisashineko@gmail.com>
2016-01-05 11:28:06 +0900

    add relations to  microposts and user

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -3,12 +3,13 @@ require 'spec_helper'
 describe Micropost do
   let(:user) { FactoryGirl.create(:user) }
 
-  before do
-    @microposts = Micropost.new(content: "Lorem ipsum", user_id: user.id)
-  end
+  before { @microposts = user.microposts.build(content: "Lorem ipsum") }
   subject { @microposts }
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
+  it { should respond_to(:user) }
+  its(:user) { should eq user}
+
   it { should be_valid }
 
   describe "when user id is not present" do
