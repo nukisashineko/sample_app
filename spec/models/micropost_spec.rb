742bf09f5a6a42bb6b0ad736112eed117d139f91
nukisashineko <nukisashineko@gmail.com>
2016-01-08 21:46:10 +0900

    add content valildation

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -16,4 +16,15 @@ describe Micropost do
     before { @microposts.user_id = nil }
     it { should_not be_valid }
   end
+
+  describe "with blank content" do
+    before { @microposts.content = " " }
+    it { should_not be_valid }
+  end
+
+
+  describe "with content that is too long" do
+    before { @microposts.content = "a"*141 }
+    it { should_not be_valid }
+  end
 end
